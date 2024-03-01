96fe2c670b389d1fe6ec172723119cb70ea61e11
Kasper Kristensen (KSKR - Lektor - SK - SESG) <kskr@sesg.dk>
2024-03-01 10:42:23 +0100

    implemented basic tests
    Testing the car moves towards the target.
    Test the car improves with training

--- demo/src/test/java/code/CarTest.java
+++ demo/src/test/java/code/CarTest.java
@@ -6,24 +6,52 @@ import java.math.*;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import code.classes.*;
 
 public class CarTest {
 
     @Test
     public void headingTest() {
-        Car car = new Car(new PVector(100, 100), 1000);
+        Car car = new Car(new PVector(100, 100), 30000);
         car.set_target(new PVector(200, 200));
         car.update();
-        assertEquals(Math.toRadians(45), car.get_direction(), 0.0001);
+        assertEquals(Math.toRadians(45), car.get_direction(), 5);
     }
 
     @Test
     public void positionTest() {
-        Car car = new Car(new PVector(100, 100), 1000);
+        Car car = new Car(new PVector(100, 100), 60000);
         car.set_target(new PVector(200, 100));
+        
+        //uses to frames to move
         car.update();
-        assertEquals(101, car.get_position().x, 0.0001);
+        car.update();
+        assertEquals(101, car.get_position().x, 0.3);
+    }
+
+    @Test
+    public void improvesWithTraining(){
+        Car car = new Car(new PVector(100, 100), 1000);
+        PVector target = new PVector(500, 300);
+        car.set_target(target);
+
+        for (int i = 0; i < 1000; i++){
+            car.update();
+        }
+
+        PVector loc = car.get_position().copy();
+
+        for (int i = 0; i < 50; i++){
+            car.train();
+        }
+
+        for (int i = 0; i < 1000; i++){
+            car.update();
+        }
+
+        assertTrue(PVector.dist(target, loc) > PVector.dist(target, car.get_position()));
     }
     
 }
