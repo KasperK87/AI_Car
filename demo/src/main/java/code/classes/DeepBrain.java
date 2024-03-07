facc519e382e397f3a6953f390b8f76f2226c856
Kasper Kristensen (KSKR - Lektor - SK - SESG) <kskr@sesg.dk>
2024-03-07 15:17:01 +0100

    implemeted a naiv test for deep brain

--- demo/src/main/java/code/classes/DeepBrain.java
+++ demo/src/main/java/code/classes/DeepBrain.java
@@ -3,9 +3,23 @@ package code.classes;
 import processing.core.PVector;
 
 public class DeepBrain extends NeuralNet {
+
+    private Perceptron[][] perceptrons;
     
-    public DeepBrain(int data_points) {
-        
+    //used for testing
+    public DeepBrain() {
+        Activation activations = new Sigmoid();
+        perceptrons = new Perceptron[2][2];
+
+        float[] w1 = {0.15f, 0.2f, 0.35f};
+        float[] w2 = {0.2f, 0.3f, 0.35f};
+        float[] w3 = {0.4f, 0.45f, 0.6f};
+        float[] w4 = {0.45f, 0.55f, 0.6f};
+
+        perceptrons[0][0] = new Perceptron(w1, activations);
+        perceptrons[0][1] = new Perceptron(w2, activations);
+        perceptrons[1][0] = new Perceptron(w3, activations);
+        perceptrons[1][1] = new Perceptron(w4, activations);
     }
 
     public void train(int data_points) {
---
+++ demo/src/test/java/DeepBrainTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import code.classes.DeepBrain;
+import processing.core.PVector;
+
+public class DeepBrainTest {
+    
+    @Test
+    public void feedforward(){
+        //following test is based mattmazur.com
+
+        float[] inputs = {0.05f, 0.1f};
+
+        DeepBrain brain = new DeepBrain();
+
+        PVector output = brain.get_direction(inputs);
+
+        assertEquals(0.75f, output.x, 0.01);
+        assertEquals(0.77f, output.y, 0.01);
+    }
+    
+}
